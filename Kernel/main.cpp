7896a74f789d2490a0266f8d2de7720a87cea3fc
Paul Scharnofske <asynts@gmail.com>
2021-03-01 16:49:55 +0100

    Kernel: Embed Shell binary into kernel image.

--- Kernel/main.cpp
+++ Kernel/main.cpp
@@ -2,10 +2,27 @@
 #include <pico/printf.h>
 #include <pico/sync.h>
 
+#include <Std/Forward.hpp>
+
+#include <elf.h>
+
+extern "C" {
+    extern u8 _binary_Shell_elf_start[];
+}
+
+void load_and_execute_shell()
+{
+    auto *header = reinterpret_cast<Elf32_Ehdr*>(_binary_Shell_elf_start);
+
+    // FIXME
+}
+
 int main() {
     stdio_init_all();
 
     printf("\033[1mBOOT\033[0m\n");
+
+    load_and_execute_shell();
 
     for(;;)
         __wfi();
