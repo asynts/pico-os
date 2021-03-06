fe42c895112435743949f75eceeb23d8592e1ec2
Paul Scharnofske <asynts@gmail.com>
2021-03-06 18:16:41 +0100

    Kernel: Simplify LoadedExecutable struct.

--- Kernel/main.cpp
+++ Kernel/main.cpp
@@ -52,16 +52,8 @@ struct LoadedExecutable {
     u32 m_stack_base;
     u32 m_stack_size;
 
-    u32 m_init_base;
     u32 m_text_base;
-    u32 m_fini_base;
-    u32 m_rodata_base;
-    u32 m_exidx_base;
-    u32 m_frame_base;
-    u32 m_init_array_base;
-    u32 m_fini_array_base;
     u32 m_data_base;
-    u32 m_bss_base;
 };
 volatile LoadedExecutable *dynamic_load_debugger_hook;
 
@@ -82,14 +74,14 @@ LoadedExecutable load_executable_into_memory(ElfWrapper elf)
     assert(data_segment.p_type == PT_LOAD);
     assert(data_segment.p_flags == PF_R | PF_W);
 
-    u32 text_base = elf.base_as_u32() + text_segment.p_offset;
-    printf("Putting text segment at %p (inplace)\n", text_base);
+    executable.m_text_base = elf.base_as_u32() + text_segment.p_offset;
+    printf("Putting text segment at %p (inplace)\n", executable.m_text_base);
 
     u8 *data = new u8[data_segment.p_memsz];
     assert(data != nullptr);
 
-    u32 data_base = u32(data);
-    printf("Putting data segment at %p (allocated)\n", data_base);
+    executable.m_data_base = u32(data);
+    printf("Putting data segment at %p (allocated)\n", executable.m_data_base);
 
     __builtin_memcpy(data, elf.base() + data_segment.p_offset, data_segment.p_filesz);
 
@@ -98,62 +90,13 @@ LoadedExecutable load_executable_into_memory(ElfWrapper elf)
 
     assert(elf.header()->e_entry >= text_segment.p_vaddr);
     assert(elf.header()->e_entry - text_segment.p_vaddr < text_segment.p_memsz);
-    executable.m_entry = text_base + (elf.header()->e_entry - text_segment.p_vaddr);
+    executable.m_entry = executable.m_text_base + (elf.header()->e_entry - text_segment.p_vaddr);
     printf("Putting entry point at %p\n", executable.m_entry);
 
     u32 stack_size = 0x1000;
     u32 stack_base = u32(new u8[stack_size]);
     u32 stack_top = stack_base + stack_top;
     printf("Putting stack top at %p\n", stack_top);
-
-    assert(elf.header()->e_shnum < SHN_LORESERVE);
-    assert(elf.header()->e_phnum < PN_XNUM);
-    for (usize section_index = 1; section_index < elf.header()->e_shnum; ++section_index) {
-        Elf32_Shdr& section = elf.sections()[section_index];
-        const char *section_name = elf.section_name_base() + section.sh_name;
-
-        if (section.sh_type == SHT_NULL)
-            continue;
-
-        bool did_understood_section = false;
-        u32 section_address;
-        if (__builtin_strcmp(section_name, ".init") == 0) {
-            section_address = executable.m_init_base = text_base + (section.sh_addr - text_segment.p_vaddr);
-            did_understood_section = true;
-        } if (__builtin_strcmp(section_name, ".text") == 0) {
-            section_address = executable.m_text_base = text_base + (section.sh_addr - text_segment.p_vaddr);
-            did_understood_section = true;
-        } if (__builtin_strcmp(section_name, ".fini") == 0) {
-            section_address = executable.m_fini_base = text_base + (section.sh_addr - text_segment.p_vaddr);
-            did_understood_section = true;
-        } if (__builtin_strcmp(section_name, ".rodata") == 0) {
-            section_address = executable.m_rodata_base = text_base + (section.sh_addr - text_segment.p_vaddr);
-            did_understood_section = true;
-        } if (__builtin_strcmp(section_name, ".ARM.exidx") == 0) {
-            section_address = executable.m_exidx_base = text_base + (section.sh_addr - text_segment.p_vaddr);
-            did_understood_section = true;
-        } if (__builtin_strcmp(section_name, ".eh_frame") == 0) {
-            section_address = executable.m_frame_base = text_base + (section.sh_addr - text_segment.p_vaddr);
-            did_understood_section = true;
-        } if (__builtin_strcmp(section_name, ".init_array") == 0) {
-            section_address = executable.m_init_array_base = data_base + (section.sh_addr - data_segment.p_vaddr);
-            did_understood_section = true;
-        } if (__builtin_strcmp(section_name, ".fini_array") == 0) {
-            section_address = executable.m_fini_array_base = data_base + (section.sh_addr - data_segment.p_vaddr);
-            did_understood_section = true;
-        } if (__builtin_strcmp(section_name, ".data") == 0) {
-            section_address = executable.m_data_base = data_base + (section.sh_addr - data_segment.p_vaddr);
-            did_understood_section = true;
-        } if (__builtin_strcmp(section_name, ".bss") == 0) {
-            section_address = executable.m_bss_base = data_base + (section.sh_addr - data_segment.p_vaddr);
-            did_understood_section = true;
-        } else {
-            printf("Skipped unknown section %s\n", section_name);
-        }
-
-        if (did_understood_section)
-            printf("Found section %s at %p\n", section_name, section_address);
-    }
 
     executable.m_stack_size = 0x1000;
     executable.m_stack_base = u32(new u8[executable.m_stack_size]);
