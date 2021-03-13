e0ca2197d62e29a178cc44152309da84ca302106
Paul Scharnofske <asynts@gmail.com>
2021-03-13 17:42:53 +0100

    Tools: Clean up EmbedFile with BufferStream class.

--- Tools/EmbedFile.cpp
+++ Tools/EmbedFile.cpp
@@ -14,50 +14,104 @@
 #include <unistd.h>
 #include <assert.h>
 
+class BufferStream {
+public:
+    BufferStream()
+    {
+        int fd = memfd_create("BufferStream", 0);
+        assert(fd >= 0);
+
+        m_file = fdopen(fd, "w");
+        assert(m_file != nullptr);
+    }
+    ~BufferStream()
+    {
+        fclose(m_file);
+    }
+
+    void write_bytes(std::span<const uint8_t> bytes)
+    {
+        size_t retval = fwrite(bytes.data(), 1, bytes.size(), m_file);
+        assert(retval == bytes.size_bytes());
+    }
+
+    template<typename T>
+    void write_object(const T& value)
+    {
+        write_bytes({ (const uint8_t*)&value, sizeof(value) });
+    }
+
+    size_t offset()
+    {
+        long offset = ftell(m_file);
+        assert(offset >= 0);
+        return (size_t)offset;
+    }
+
+    void seek(size_t offset)
+    {
+        int retval = fseek(m_file, offset, SEEK_SET);
+        assert(retval == 0);
+    }
+
+    void seek_relative(ssize_t offset)
+    {
+        int retval = fseek(m_file, offset, SEEK_CUR);
+        assert(retval == 0);
+    }
+
+    void copy_to(int fd)
+    {
+        ssize_t retval;
+
+        retval = fseek(m_file, 0, SEEK_END);
+        assert(retval == 0);
+
+        size_t size = offset();
+
+        retval = fseek(m_file, 0, SEEK_SET);
+        assert(retval == 0);
+
+        retval = copy_file_range(fileno(m_file), nullptr, fd, nullptr, size, 0);
+        assert(retval == size);
+    }
+
+private:
+    FILE *m_file;
+};
+
 class ELF {
 public:
     ELF()
     {
-        int fd = memfd_create("memfd:elf", 0);
-        assert(fd >= 0);
-
-        m_data = fdopen(fd, "w");
-        assert(m_data != nullptr);
-
-        int retval = fseek(m_data, sizeof(Elf32_Ehdr), SEEK_CUR);
-        assert(retval == 0);
+        m_stream.seek(sizeof(Elf32_Ehdr));
 
         Elf32_Shdr shdr;
         shdr.sh_type = SHT_NULL;
         shdr.sh_name = append_section_name("");
         m_sections.push_back(shdr);
     }
-    ~ELF()
-    {
-        int retval = fclose(m_data);
-        assert(retval == 0);
-    }
 
     Elf32_Shdr& append_section(std::string_view name, std::span<const uint8_t> data)
     {
         Elf32_Shdr shdr;
-        shdr.sh_addr = ftell(m_data) - sizeof(Elf32_Ehdr);
+        shdr.sh_addr = m_stream.offset() - sizeof(Elf32_Ehdr);
         shdr.sh_addralign = 4;
         shdr.sh_entsize = 0;
         shdr.sh_flags = SHF_ALLOC;
         shdr.sh_info = 0;
         shdr.sh_link = 0;
         shdr.sh_name = append_section_name(name);
-        shdr.sh_offset = ftell(m_data);
+        shdr.sh_offset = m_stream.offset();
         shdr.sh_size = data.size();
         shdr.sh_type = SHT_PROGBITS;
         
-        write_bytes(data);
+        m_stream.write_bytes(data);
 
         return m_sections.emplace_back(shdr);
     }
 
-    FILE* finalize()
+    BufferStream& finalize()
     {
         assert(!m_finalized);
         m_finalized = true;
@@ -68,8 +122,8 @@ public:
 
         encode_header(section_offset, shstrtab_section_index);
 
-        fseek(m_data, 0, SEEK_SET);
-        return m_data;
+        m_stream.seek(0);
+        return m_stream;
     }
 
 private:
@@ -79,18 +133,6 @@ private:
         m_shstrtab << name;
         m_shstrtab << '\0';
         return offset;
-    }
-
-    template<typename T>
-    void write(const T& value)
-    {
-        write_bytes({ (const uint8_t*)&value, sizeof(value) });
-    }
-
-    void write_bytes(std::span<const uint8_t> data)
-    {
-        size_t retval = fwrite(data.data(), 1, data.size_bytes(), m_data);
-        assert(retval == data.size_bytes());
     }
 
     void append_shstrtab_section(size_t& shstrtab_section_index)
@@ -105,10 +147,10 @@ private:
         shdr.sh_type = SHT_STRTAB;
 
         std::string shstrtab = m_shstrtab.str();
-        shdr.sh_offset = ftell(m_data);
-        shdr.sh_addr = ftell(m_data) - sizeof(Elf32_Ehdr);
+        shdr.sh_offset = m_stream.offset();
+        shdr.sh_addr = m_stream.offset() - sizeof(Elf32_Ehdr);
         shdr.sh_size = shstrtab.size();
-        write_bytes({ (const uint8_t*)shstrtab.data(), shstrtab.size() });
+        m_stream.write_bytes({ (const uint8_t*)shstrtab.data(), shstrtab.size() });
 
         shstrtab_section_index = m_sections.size();
         m_sections.push_back(shdr);
@@ -118,10 +160,10 @@ private:
     {
         append_shstrtab_section(shstrtab_section_index);
 
-        section_offset = ftell(m_data);
+        section_offset = m_stream.offset();
 
         for (const Elf32_Shdr& section : m_sections)
-            write(section);
+            m_stream.write_object(section);
     }
 
     void encode_header(size_t section_offset, size_t shstrtab_section_index)
@@ -149,12 +191,13 @@ private:
         ehdr.e_type = ET_REL;
         ehdr.e_version = EV_CURRENT;
 
-        fseek(m_data, 0, SEEK_SET);
-        write(ehdr);
+        m_stream.seek(0);
+        m_stream.write_object(ehdr);
     }
 
+    BufferStream m_stream;
+
     bool m_finalized = false;
-    FILE *m_data;
     std::vector<Elf32_Shdr> m_sections;
     std::ostringstream m_shstrtab;
 };
@@ -177,16 +220,15 @@ std::span<const uint8_t> mmap_file(std::filesystem::path path)
 
 int main() {
     ELF elf;
-
     elf.append_section(".embedded.binary", mmap_file("Userland/Shell.elf"));
 
-    FILE *fp = elf.finalize();
+    BufferStream& elf_binary = elf.finalize();
 
-    struct stat statbuf;
-    fstat(fileno(fp), &statbuf);
+    int output_fd = open("Shell.embedded.elf", O_WRONLY);
+    assert(output_fd >= 0);
 
-    FILE *output = fopen("Shell.embedded.elf", "w");
-    ssize_t retval = copy_file_range(fileno(fp), nullptr, fileno(output), nullptr, statbuf.st_size, 0);
-    assert(retval == statbuf.st_size);
-    fclose(output);
+    elf_binary.copy_to(output_fd);
+
+    int retval = close(output_fd);
+    assert(retval == 0);
 }
