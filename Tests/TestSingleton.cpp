36ba15fd8fd5931a5d3c181b80f3e5c6b9384a28
Paul Scharnofske <asynts@gmail.com>
2021-04-02 12:44:59 +0200

    Tests: Add unit tests for Std::Singleton

---
+++ Tests/TestSingleton.cpp
@@ -0,0 +1,36 @@
+#include <Tests/TestSuite.hpp>
+
+#include <Std/Singleton.hpp>
+
+class A : public Std::Singleton<A> {
+public:
+    A(const A&)
+    {
+        ASSERT_NOT_REACHED();
+    }
+    A(A&&)
+    {
+        ASSERT_NOT_REACHED();
+    }
+    ~A()
+    {
+        ASSERT_NOT_REACHED();
+    }
+
+private:
+    friend Singleton<A>;
+    A()
+    {
+        static bool initialized = false;
+
+        ASSERT(!initialized);
+        initialized = true;
+    }
+};
+
+TEST_CASE(singleton)
+{
+    A::the();
+}
+
+TEST_MAIN();
