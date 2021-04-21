3c31172538bce35c5a070e884de82cc00cf4247b
Paul Scharnofske <asynts@gmail.com>
2021-04-21 18:00:42 +0200

    Userland: Implement constructors (somewhat)
    This does not work.
    
    My understanding is that the linker or compiler doesn't know how to
    generate init-array tables with '-fropi' and '-frwpi' active.
    
    Not sure if this is fixable.

--- Userland/LibC/sys/crt0.c
+++ Userland/LibC/sys/crt0.c
@@ -1,10 +1,11 @@
 #include <stdint.h>
 #include <string.h>
+#include <stdio.h>
 
 extern uint8_t __bss_start__[];
 extern uint8_t __bss_end__[];
 
-typedef void (*callback_t)();
+typedef void (*callback_t)(void);
 
 extern callback_t __preinit_array_start[];
 extern callback_t __preinit_array_end[];
@@ -15,21 +16,27 @@ extern callback_t __init_array_end[];
 extern callback_t __fini_array_start[];
 extern callback_t __fini_array_end[];
 
-static void call_callback_array(callback_t *start, callback_t *end)
-{
-    for (callback_t *callback = start; callback < end; callback++)
-        (*callback)();
-}
-
 void _init()
 {
     memset(__bss_start__, 0, __bss_end__ - __bss_start__);
 
-    call_callback_array(__preinit_array_start, __preinit_array_end);
-    call_callback_array(__init_array_start, __init_array_end);
+    printf("[_init] calling preinit-array\n");
+    for (callback_t *callback = __preinit_array_start; callback < __preinit_array_end; ++callback) {
+        printf("[_init] callback=%p *callback=%p\n", callback, *callback);
+        (*callback)();
+    }
+
+    printf("[_init] calling init-array\n");
+    for (callback_t *callback = __init_array_start; callback < __init_array_end; ++callback) {
+        callback = (callback_t*)((uint32_t)callback & ~1);
+
+        printf("[_init] callback=%p\n", callback);
+        printf("[_init] *callback=%p\n", *callback);
+        (*callback)();
+    }
 }
 
 void _fini()
 {
-    call_callback_array(__fini_array_start, __fini_array_end);
+    // call_callback_array((callback_t *const)__fini_array_start, (callback_t *const)__fini_array_end);
 }
