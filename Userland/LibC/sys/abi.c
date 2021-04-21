778cea21c1312a02d7ecd41025717b167c822a2b
Paul Scharnofske <asynts@gmail.com>
2021-04-21 17:39:43 +0200

    Userland: Turns out memset4 is not provided by the OS

--- Userland/LibC/sys/abi.c
+++ Userland/LibC/sys/abi.c
@@ -1,27 +1,42 @@
 #include <stdint.h>
+#include <assert.h>
 
-#define rom_func_table ((uint16_t*)0x00000014)
-#define rom_data_table ((uint16_t*)0x00000016)
-#define rom_table_lookup_ptr ((void*(*)(uint16_t*, uint32_t))0x00000014)
+#define rom_table_code(code1, code2) ((code2) << 8 | (code1))
 
-void* rom_func_lookup(uint32_t code)
+#define ROM_MEMCPY 0
+#define ROM_MEMSET 1
+#define ROM_MAX 2
+
+static int rom_functions_initialized = 0;
+static uint32_t rom_functions[] = {
+    [ROM_MEMCPY] = rom_table_code('M', 'C'),
+    [ROM_MEMSET] = rom_table_code('M', 'S'),
+};
+
+static void rom_functions_init()
 {
-    return (*rom_table_lookup_ptr)(rom_func_table, code);
-}
+    if (rom_functions_initialized)
+        return;
+    rom_functions_initialized = 1;
 
-void* rom_data_lookup(uint32_t code)
-{
-    return (*rom_table_lookup_ptr)(rom_data_table, code);
+    typedef uint32_t (*fn_table_lookup)(uint16_t *table, uint32_t code);
+
+    uint16_t *table_lookup_ptr = (uint16_t*)0x00000018;
+    fn_table_lookup table_lookup = (fn_table_lookup)(uint32_t)*table_lookup_ptr;
+
+    uint16_t *func_table_ptr = (uint16_t*)0x00000014;
+    uint16_t *func_table = (uint16_t*)(uint32_t)*func_table_ptr;
+
+    for (size_t i = 0; i < ROM_MAX; ++i) {
+        uint32_t value = rom_functions[i] = table_lookup(func_table, rom_functions[i]);
+        assert(value);
+    }
 }
 
 void __aeabi_memcpy(void *dest, const void *src, size_t n)
 {
-    typedef void (*fn_memcpy)(void*, const void*, size_t);
-    return ( (fn_memcpy)rom_func_lookup('M' << 8 | 'C') )(dest, src, n);
-}
+    rom_functions_init();
 
-void __aeabi_memcpy4(void *dest, const void *src, size_t n)
-{
-    typedef void (*fn_memcpy4)(void*, const void*, size_t);
-    return ( (fn_memcpy4)rom_func_lookup('C' << 8 | '4') )(dest, src, n);
+    typedef void (*fn_memcpy)(void*, const void*, size_t);
+    ((fn_memcpy)rom_functions[ROM_MEMCPY])(dest, src, n);
 }
--- Userland/LibC/sys/abi.h
+++ Userland/LibC/sys/abi.h
@@ -1,4 +1,3 @@
 #pragma once
 
 void __aeabi_memcpy(void *dest, const void *src, size_t n);
-void __aeabi_memcpy4(void *dest, const void *src, size_t n);
