0e23a175bfc90858f0b29ee7bf9739d89c10b087
Paul Scharnofske <asynts@gmail.com>
2022-01-20 14:40:46 +0100

    Prototype+Boot: Implement the 'checksum.py' script
    -   I still need to verify that this checksum is accepted by the actual
        hardware.

--- Prototype/Boot/build.sh
+++ Prototype/Boot/build.sh
@@ -10,5 +10,7 @@ LDFLAGS=""
 [[ -d build ]] || mkdir -p build
 "$ASM" $ASMFLAGS boot_2_flash_second_stage.S -o build/boot_2_flash_second_stage.S.o
 
+python3 scripts/checksum.py build/boot_2_flash_second_stage.S.o build/boot_2_flash_second_stage.with-checksum.o
+
 "$LD" $LDFLAGS -o build/system.elf -T link.ld \
-    build/boot_2_flash_second_stage.S.o
+    build/boot_2_flash_second_stage.with-checksum.o
