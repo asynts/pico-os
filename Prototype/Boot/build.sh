ad46f0e292ef92b2ff2644d275c3905fc99ea472
Paul Scharnofske <asynts@gmail.com>
2022-01-20 13:36:43 +0100

    Prototype+Boot: Verify that the linker script works

---
+++ Prototype/Boot/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+ASM="arm-none-eabi-as"
+ASMFLAGS="-mcpu=cortex-m0plus"
+
+LD="arm-none-eabi-ld"
+LDFLAGS=""
+
+[[ -d build ]] || mkdir -p build
+"$ASM" $ASMFLAGS boot_2_flash_second_stage.S -o build/boot_2_flash_second_stage.S.o
+
+"$LD" $LDFLAGS -o build/system.elf -T link.ld \
+    build/boot_2_flash_second_stage.S.o
