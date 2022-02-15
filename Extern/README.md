3cf654b08136364509ff7fe23d9d0a0500ebd05c
Paul Scharnofske <asynts@gmail.com>
2022-02-15 09:31:50 +0100

    Build: Add PicoSDK as submodule
    The idea is to use the hardware register definitions.  Currently, I
    haven't updated the calls but this appears to work at first sight.

---
+++ Extern/README.md
@@ -0,0 +1,4 @@
+We include a few files from the original Pico SDK.
+It is very annoying to declare all the hardware registers by myself.
+
+The goal is to import as little as possible, therefore, we are patching some files in `/Extern/include`.
--- Sources/Kernel/Drivers/UartDriver.cpp
+++ Sources/Kernel/Drivers/UartDriver.cpp
@@ -2,110 +2,11 @@
 
 #include <Kernel/Drivers/UartDriver.hpp>
 
-// FIXME: Move this into 'Hardware/Dma.h'.
+#include <hardware/structs/uart.h>
+#include <hardware/structs/dma.h>
 
-#define DMA_BASE                                0x50000000
-#define DMA_CHANNEL0_CTRL                            0x010
-#define DMA_CHANNEL0_CTRL_TRIGGER                    0x00c
-#define DMA_CHANNEL0_READ_ADDR                       0x000
-#define DMA_CHANNEL0_WRITE_ADDR                      0x004
-#define DMA_CHANNEL0_TRANSACTION_COUNT               0x008
-#define DMA_CHANNEL0_TRANSACTION_COUNT_TRIGGER       0x01c
-
-struct DmaCtrl {
-    u32 enabled : 1;
-    u32 high_priority : 1;
-    u32 data_size : 2;
-    u32 increment_read : 1;
-    u32 increment_write : 1;
-    u32 ring_size : 4;
-    u32 ring_select : 1;
-    u32 chain_to : 4;
-    u32 transfer_request_select : 6;
-    u32 irq_quiet : 1;
-    u32 byte_swap : 1;
-    u32 sniff_enabled : 1;
-    u32 busy : 1;
-    u32 _reserved : 4;
-    u32 write_error : 1;
-    u32 read_error : 1;
-    u32 error : 1;
-};
-static_assert(sizeof(DmaCtrl) == 4);
-
-DmaCtrl *dma_channel0_ctrl = reinterpret_cast<DmaCtrl*>(DMA_BASE + DMA_CHANNEL0_CTRL);
-DmaCtrl *dma_channel0_ctrl_trigger = reinterpret_cast<DmaCtrl*>(DMA_BASE + DMA_CHANNEL0_CTRL_TRIGGER);
-
-// FIXME: Get rid of many 'reinterpret_cast' calls by changing this to 'u8**'.
-uptr    *dma_channel0_read_addr                 = reinterpret_cast<uptr*>(DMA_BASE + DMA_CHANNEL0_READ_ADDR);
-// FIXME: Get rid of many 'reinterpret_cast' calls by changing this to 'u8**'.
-uptr    *dma_channel0_write_addr                = reinterpret_cast<uptr*>(DMA_BASE + DMA_CHANNEL0_WRITE_ADDR);
-
-usize   *dma_channel0_transaction_count         = reinterpret_cast<usize*>(DMA_BASE + DMA_CHANNEL0_TRANSACTION_COUNT);
-usize   *dma_channel0_transaction_count_trigger = reinterpret_cast<usize*>(DMA_BASE + DMA_CHANNEL0_TRANSACTION_COUNT_TRIGGER);
-
-// FIXME: Move this into 'Hardware/Uart.hpp'.
-
-#define UART0_BASE                  0x40034000
-#define UART_DATA                        0x000
-#define UART_CONTROL                     0x030
-#define UART_INTEGER_BAUDRATE            0x024
-#define UART_FRACTIONAL_BAUDRATE         0x028
-#define UART_LINE_CONTROL                0x02c
-#define UART_DMA_CONTROL                 0x048
-#define UART_INTERRUPT_MASK              0x038
-
-struct UartLineControl {
-    u32 send_break : 1;
-    u32 parity_enabled : 1;
-    u32 even_parity_select : 1;
-    u32 two_stop_bit_select : 1;
-    u32 fifo_enabled : 1;
-    u32 word_length : 2;
-    u32 stick_parity_select : 1;
-    u32 _reserved_1 : 24;
-};
-static_assert(sizeof(UartLineControl) == 4);
-
-struct UartControl {
-    u32 uart_enabled : 1;
-    u32 sir_enabled : 1;
-    u32 sir_low_power : 1;
-    u32 _reserved_1 : 4;
-    u32 loopback_enabled : 1;
-    u32 transmit_enabled : 1;
-    u32 receive_enabled : 1;
-    u32 data_transmit_ready : 1;
-    u32 request_to_send : 1;
-    u32 output_1 : 1;
-    u32 output_2 : 1;
-    u32 rts_flow_control_enabled : 1;
-    u32 cts_flow_control_enabled : 1;
-    u32 _reserved_2 : 16;
-};
-static_assert(sizeof(UartControl) == 4);
-
-struct UartDmaControl {
-    u32 receive_dma_enabled : 1;
-    u32 transmit_dma_enabled : 1;
-    u32 dma_on_error : 1;
-    u32 _reserved_1 : 29;
-};
-static_assert(sizeof(UartDmaControl) == 4);
-
-struct UartInterruptMask {
-    u32 _unknown_1 : 11;
-    u32 _reserved_1 : 21;
-};
-static_assert(sizeof(UartInterruptMask) == 4);
-
-u32                 *uart0_integer_baudrate     = reinterpret_cast<u32*>(UART0_BASE + UART_INTEGER_BAUDRATE);
-u32                 *uart0_fractional_baudrate  = reinterpret_cast<u32*>(UART0_BASE + UART_FRACTIONAL_BAUDRATE);
-UartLineControl     *uart0_line_control         = reinterpret_cast<UartLineControl*>(UART0_BASE + UART_LINE_CONTROL);
-UartControl         *uart0_control              = reinterpret_cast<UartControl*>(UART0_BASE + UART_CONTROL);
-UartDmaControl      *uart0_dma_control          = reinterpret_cast<UartDmaControl*>(UART0_BASE + UART_DMA_CONTROL);
-UartInterruptMask   *uart0_interrupt_mask       = reinterpret_cast<UartInterruptMask*>(UART0_BASE + UART_INTERRUPT_MASK);
 // FIXME: Verify that we don't accidently trigger the channel.
+// FIXME: verify that all race conditions have been resolved.
 
 namespace Kernel::Drivers
 {
